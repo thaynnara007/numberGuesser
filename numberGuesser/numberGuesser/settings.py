3fdeee92f090775c2a4825873caafde29ef2d1e7
thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2020-11-02 09:39:34 -0300

    add key

--- numberGuesser/numberGuesser/settings.py
+++ numberGuesser/numberGuesser/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'idxn5g9wufabk!(e8(8r19z-&3sg-=-9)w59c6oujiagaf$45_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
